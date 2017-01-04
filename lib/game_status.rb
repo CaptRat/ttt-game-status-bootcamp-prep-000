cca0e1525e59353974550d2241df5609c973f3e6
Patrick Logan <jpatricklogan@gmail.com>
2017-01-04 18:54:18 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,33 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], #bottom row
+  [0,3,6], #left columns
+  [1,4,7], #Middle
+  [2,5,8], #right columns
+  [0,4,8], #diagonal
+  [2,4,6] #diagonal
+]
+
+def won(board)
+  WIN_COMBINATIONS.detect{|board| board.include?}
+end
+
+# def full
+# earn
+# end
+#
+# def draw
+#
+# end
+#
+# def over
+#
+# end
+#
+# def winner
+#
+# end
