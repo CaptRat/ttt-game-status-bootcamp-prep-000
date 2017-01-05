d8a7ebd791e72eb9f286116d8a7e37c5f82d7114
Patrick Logan <jpatricklogan@gmail.com>
2017-01-05 20:04:54 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,25 +3,31 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+# Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], #top row
-  [3,4,5], #middle row
+  [0,1,2], #Top row
+  [3,4,5], #Middle row
   [6,7,8], #bottom row
-  [0,3,6], #left columns
+  [0,3,6], #left column
   [1,4,7], #Middle
-  [2,5,8], #right columns
+  [2,5,8], #right column
   [0,4,8], #diagonal
   [2,4,6]  #diagonal
 ]
-# Track whose move it is: set up counter (1, 3, 5, 7 moves are X; 2, 4, 6 are O)
-# board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-def won?(board)
-   WIN_COMBINATIONS.detect(board[index]= "X")
-   board.detect
+
+
+combo_1 = win_combo[0]
+combo_2 = win_combo[1]
+combo_3 = win_combo[2]
+
+def won(board)
+    WIN_COMBINATIONS.each do
+      [win_combo]
+    end
 end
 
 # def full
-# board.select{|token| token.is_a?"X"}
+#
 # end
 #
 # def draw
