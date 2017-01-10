25f4777827329347a3fd8ecd6b8c031977d3fb88
Patrick Logan <jpatricklogan@gmail.com>
2017-01-10 01:28:57 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,23 +15,6 @@ WIN_COMBINATIONS = [
   [2,4,6]  #diagonal
 ]
 
-# def won?(board)
-#   WIN_COMBINATIONS.all?{|index| board[index]=="X"} || {|index| board[index] == "O"}
-# end
-# def won?(board)
-#   WIN_COMBINATIONS.detect {|c| c.all?{|p| board[p]=="X"} || c.all?{|p| board[p]=="O"}}
-# end
-# def won?(board)
-#    WIN_COMBINATIONS.detect do |win_combo|
-#      combo_1 = win_combo[0]
-#      combo_2 = win_combo[1]
-#      combo_3 = win_combo[2]
-#
-#      position_1 = board[combo_1]
-#      position_2 = board[combo_2]
-#      position_3 = board[combo_3]
-#    end
-# end
 def won?(board)
   WIN_COMBINATIONS.each do |combo|
 	  if combo.all? {|index| board[index]=="O"} || combo.all? {|index| board[index]=="X"}
